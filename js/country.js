008a01443bf97caaa3bb9e882deb7092136ac1de
AnibalNiz4 <anibal.niz@hotmail.com>
2021-07-29 13:08:14 -0300

    rest countries api

---
+++ js/country.js
@@ -0,0 +1,106 @@
+const back = document.querySelector('.back');
+const country_flag = document.querySelector('.country_flag');
+const country_tittle = document.querySelector('.country_tittle');
+const native = document.querySelector('.native');
+const popu = document.querySelector('.popu');
+const regi = document.querySelector('.regi');
+const sub = document.querySelector('.sub');
+const capi = document.querySelector('.capi');
+const tope = document.querySelector('.top');
+const curr = document.querySelector('.curr');
+const lang = document.querySelector('.lang');
+const container_borders = document.querySelector('.container_borders');
+
+//light mode
+const dark_mode = document.querySelector('.dark_mode');
+const bkg_nav = document.querySelector('.bkg_nav');
+const container = document.querySelector('.container');
+const back_arrow = document.querySelector('.back_arrow');
+
+//dark or light page
+const toggleMode = localStorage.getItem("darkMode");
+
+if(toggleMode == "light_mode"){
+    bkg_nav.classList.toggle("light_mode");
+    container.classList.toggle("light_mode");
+    document.body.style.background = 'rgb(245, 245, 245)';
+    back_arrow.src = '/scss/img/arrow_back_black_24dp.svg';
+}
+else if(toggleMode == "dark_mode"){
+    bkg_nav.classList.toggle("dark_mode");
+    container.classList.toggle("dark_mode");
+    document.body.style.background = 'rgb(32, 44, 55)';
+    back_arrow.src = '/scss/img/arrow_back_white_24dp.svg';
+}
+
+dark_mode.addEventListener('click', () =>{
+
+    if(bkg_nav.classList.contains('light_mode')){
+        bkg_nav.classList.toggle("light_mode");
+        container.classList.toggle("light_mode");
+        bkg_nav.classList.toggle("dark_mode");
+        container.classList.toggle("dark_mode");
+        document.body.style.background = 'rgb(32, 44, 55)';
+        back_arrow.src = '/scss/img/arrow_back_white_24dp.svg';
+        localStorage.setItem("darkMode", "dark_mode");
+    }
+    else{
+        bkg_nav.classList.toggle("light_mode");
+        container.classList.toggle("light_mode");
+        bkg_nav.classList.toggle("dark_mode");
+        container.classList.toggle("dark_mode");
+        document.body.style.background = 'rgb(245, 245, 245)';
+        back_arrow.src = '/scss/img/arrow_back_black_24dp.svg';
+        localStorage.setItem("darkMode", "light_mode");
+    }
+});
+
+let dom = window.location.search;
+let cod = dom.slice(1);
+
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res => res.json())
+.then(data => {
+    if (data.code === 422) {
+        alert('no anda we') 
+    }
+    else{
+        for(let i = 0; i<data.length; i++){
+            if(cod == data[i].alpha3Code){
+                let flag = data[i].flag;
+                country_flag.style.backgroundImage = `url(${flag})`;
+                country_tittle.innerHTML = data[i].name;
+                native.innerHTML = data[i].nativeName;
+                popu.innerHTML = data[i].population;
+                regi.innerHTML = data[i].region;
+                sub.innerHTML = data[i].subregion;
+                capi.innerHTML = data[i].capital;
+                tope.innerHTML = data[i].topLevelDomain;
+                curr.innerHTML = data[i].currencies[0].name;
+                for(let j = 0; j<data[i].languages.length; j++){
+                    lang.innerHTML += data[i].languages[j].name;
+                    if(j<data[i].languages.length-1){
+                        lang.innerHTML += ', ';
+                    }
+                }
+                for(let b = 0; b<data[i].borders.length; b++){
+                    createBorder(data[i].borders[b], data[i].borders[b]);
+                }
+            }
+        }
+    }
+});
+
+const createBorder = (bor, cod) =>{
+    const border = document.createElement('a');
+
+    border.classList.add('borders');
+    border.textContent = bor;
+    border.href = 'country.html?'+cod;
+
+    container_borders.appendChild(border);
+}
+
+back.addEventListener('click', () =>{
+    window.history.back();
+})
---
+++ js/script.js
@@ -0,0 +1,236 @@
+// appendChild to countrys
+const countrys = document.querySelector('.countrys');
+
+//filter
+const arrow_filter = document.querySelector('.arrow_filter');
+const countrys_filter = document.querySelector('.countrys_filter');
+
+const africa = document.querySelector('.africa');
+const america = document.querySelector('.america');
+const asia = document.querySelector('.asia');
+const europe = document.querySelector('.europe');
+const oceania = document.querySelector('.oceania');
+const all = document.querySelector('.all');
+
+//input search
+const button = document.querySelector('.button');
+const input = document.querySelector('.input_text');
+
+//light mode
+const dark_mode = document.querySelector('.dark_mode');
+const bkg_nav = document.querySelector('.bkg_nav');
+const container = document.querySelector('.container');
+
+//dark or light page
+const toggleMode = localStorage.getItem("darkMode");
+
+if(toggleMode == "light_mode"){
+    bkg_nav.classList.toggle("light_mode");
+    container.classList.toggle("light_mode");
+    document.body.style.background = 'rgb(245, 245, 245)';
+    arrow_filter.src = '/scss/img/expand_more_black_24dp.svg';
+}
+else if(toggleMode == "dark_mode"){
+    bkg_nav.classList.toggle("dark_mode");
+    container.classList.toggle("dark_mode");
+    document.body.style.background = 'rgb(32, 44, 55)';
+    arrow_filter.src = '/scss/img/expand_more_white_24dp.svg';
+}
+
+dark_mode.addEventListener('click', () =>{
+
+    if(bkg_nav.classList.contains('light_mode')){
+        bkg_nav.classList.toggle("light_mode");
+        container.classList.toggle("light_mode");
+        bkg_nav.classList.toggle("dark_mode");
+        container.classList.toggle("dark_mode");
+        document.body.style.background = 'rgb(32, 44, 55)';
+        arrow_filter.src = '/scss/img/expand_more_white_24dp.svg';
+        localStorage.setItem("darkMode", "dark_mode");
+    }
+    else{
+        bkg_nav.classList.toggle("light_mode");
+        container.classList.toggle("light_mode");
+        bkg_nav.classList.toggle("dark_mode");
+        container.classList.toggle("dark_mode");
+        document.body.style.background = 'rgb(245, 245, 245)';
+        arrow_filter.src = '/scss/img/expand_more_black_24dp.svg';
+        localStorage.setItem("darkMode", "light_mode");
+    }
+});
+
+arrow_filter.addEventListener('click', () =>{
+    if(countrys_filter.style.display == 'flex'){
+        countrys_filter.style.display = 'none';
+    }
+    else{
+        countrys_filter.style.display = 'flex';
+    }
+});
+
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res => res.json())
+.then(data => {
+    if (data.code === 422) {
+        alert('no anda we') 
+    }
+    else{
+        for(let i = 0; i<data.length; i++){
+            createCountry(data[i].alpha3Code, data[i].flag, data[i].name, data[i].population, data[i].region, data[i].capital);
+        }
+    }
+});
+
+const createCountry = (cod, img, name, pop, reg, cap) =>{
+    const a = document.createElement('a');
+    const country_data = document.createElement('div');
+    const flag = document.createElement('div');
+    const country_text = document.createElement('div');
+    const h4 = document.createElement('h4');
+    const population = document.createElement('div');
+    const pop_text = document.createElement('p');
+    const pop_data = document.createElement('p');
+    const region = document.createElement('div');
+    const reg_text = document.createElement('p');
+    const reg_data = document.createElement('p');
+    const capital = document.createElement('div');
+    const cap_text = document.createElement('p');
+    const cap_data = document.createElement('p');
+
+
+    country_data.classList.add('country_data');
+    flag.classList.add('flag');
+    country_text.classList.add('country_text');
+    h4.classList.add('country_name');
+    population.classList.add('population');
+    pop_data.classList.add('population_data');
+    region.classList.add('region');
+    reg_data.classList.add('region_data');
+    capital.classList.add('capital');
+    cap_data.classList.add('capital_data');
+
+
+    a.href = 'country.html?'+cod;
+    pop_text.textContent = 'Population:'
+    reg_text.textContent = 'Region:'
+    cap_text.textContent = 'Capital:'
+    flag.style.backgroundImage = `url(${img})`;
+    h4.textContent = name;
+    pop_data.textContent = pop;
+    reg_data.textContent = reg;
+    cap_data.textContent = cap;
+
+
+    countrys.appendChild(a);
+    a.appendChild(country_data);
+    country_data.appendChild(flag);
+    country_data.appendChild(country_text);
+    country_text.appendChild(h4);
+    country_text.appendChild(population);
+    population.appendChild(pop_text);
+    population.appendChild(pop_data);
+    country_text.appendChild(region);
+    region.appendChild(reg_text);
+    region.appendChild(reg_data);
+    country_text.appendChild(capital);
+    capital.appendChild(cap_text);
+    capital.appendChild(cap_data);
+}
+
+africa.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        if(countrys.children[i].children[0].lastElementChild.childNodes[2].childNodes[1].innerText == 'Africa'){
+            countrys.children[i].style.display = 'inline-block';
+        }
+        else{
+            countrys.children[i].style.display = 'none';
+        }
+    }
+    countrys_filter.style.display = 'none';
+});
+
+america.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        if(countrys.children[i].children[0].lastElementChild.childNodes[2].childNodes[1].innerText == 'Americas'){
+            countrys.children[i].style.display = 'inline-block';
+        }
+        else{
+            countrys.children[i].style.display = 'none';
+        }
+    }
+    countrys_filter.style.display = 'none';
+});
+
+asia.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        if(countrys.children[i].children[0].lastElementChild.childNodes[2].childNodes[1].innerText == 'Asia'){
+            countrys.children[i].style.display = 'inline-block';
+        }
+        else{
+            countrys.children[i].style.display = 'none';
+        }
+    }
+    countrys_filter.style.display = 'none';
+});
+
+europe.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        if(countrys.children[i].children[0].lastElementChild.childNodes[2].childNodes[1].innerText == 'Europe'){
+            countrys.children[i].style.display = 'inline-block';
+        }
+        else{
+            countrys.children[i].style.display = 'none';
+        }
+    }
+    countrys_filter.style.display = 'none';
+});
+
+oceania.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        if(countrys.children[i].children[0].lastElementChild.childNodes[2].childNodes[1].innerText == 'Oceania'){
+            countrys.children[i].style.display = 'inline-block';
+        }
+        else{
+            countrys.children[i].style.display = 'none';
+        }
+    }
+    countrys_filter.style.display = 'none';
+});
+
+all.addEventListener('click', () =>{
+    for(let i = 0; i<countrys.children.length; i++){
+        countrys.children[i].style.display = 'inline-block';
+    }
+    countrys_filter.style.display = 'none';
+});
+
+
+
+button.addEventListener('click', (e) =>{
+    e.preventDefault();
+});
+
+input.addEventListener('keyup', (e) =>{
+    let input_value = e.path[0].value;
+    input_value = input_value.toLowerCase();
+
+    if(input_value.length > 0){
+        for(let i = 0; i<countrys.children.length; i++){
+            let info = countrys.children[i].children[0].lastElementChild.firstElementChild.innerText;
+            info = info.toLowerCase();
+            let res = info.indexOf(input_value);
+            if(res != -1){
+                countrys.children[i].style.display = 'inline-block';
+            }
+            else if(res == -1){
+                countrys.children[i].style.display = 'none';
+            }
+        }
+    }
+
+    if(input_value == 0){
+        for(let i = 0; i<countrys.children.length; i++){
+            countrys.children[i].style.display = 'inline-block';
+        }
+    }
+});
